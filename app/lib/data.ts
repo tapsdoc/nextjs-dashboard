585f5731e243cf8a8fba6965f2f152f0d46f5b36
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2024-05-25 13:17:57 +0200

    Complete dashboard chats

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,8 +5,8 @@ import {
 	InvoiceForm,
 	InvoicesTable,
 	LatestInvoiceRaw,
-	User,
-	Revenue
+	Revenue,
+	User
 } from './definitions';
 import { formatCurrency } from './utils';
 
@@ -28,12 +28,11 @@ export async function fetchLatestInvoices() {
 			FROM invoices
 			JOIN customers ON invoices.customer_id = customers.id
 			ORDER BY invoices.date DESC LIMIT 5`;
-		
-		const latestInvoices = data.rows.map((invoice) => ({
+
+		return data.rows.map((invoice) => ({
 			...invoice,
 			amount: formatCurrency(invoice.amount)
 		}));
-		return latestInvoices;
 	} catch (error) {
 		console.error('Database Error:', error);
 		throw new Error('Failed to fetch the latest invoices.');
@@ -44,11 +43,11 @@ export async function fetchCardData() {
 	try {
 		const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
 		const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-		const invoiceStatusPromise =
-			sql`
-				SELECT SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-			   SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-				FROM invoices`;
+		const invoiceStatusPromise = sql`
+			SELECT SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+			SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+			FROM invoices
+		`;
 		
 		const data = await Promise.all([
 			invoiceCountPromise,
