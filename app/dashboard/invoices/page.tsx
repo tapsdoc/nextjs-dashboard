78772ecbe4aff73e4d8349757e3b4febebec948f
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2024-05-25 18:54:58 +0200

    code cleanup

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InvoicesPage = () => {
 	return (
-		<div className="text-blue-500">
+		<div className="text-blue-500 font-bold text-7xl">
 			Invoices Page
 		</div>
 	);
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,18 +11,10 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-export async function fetchRevenue() {
+export async function fetchRevenue(){
 	noStore();
 	try {
-		// We artificially delay a response for demo purposes.
-		// Don't do this in production :)
-		console.log('Fetching revenue data...');
-		await new Promise((resolve) => setTimeout(resolve, 3000));
-		
 		const data = await sql<Revenue>`SELECT * FROM revenue`;
-		console.log('Data fetch completed after 3 seconds.');
-		console.log(data.rows);
-		
 		return data.rows;
 	} catch (error) {
 		console.error('Database Error:', error);
