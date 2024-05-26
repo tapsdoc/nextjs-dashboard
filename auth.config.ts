727101e877e78f588cd1125a42935fe5bad1198e
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2024-05-26 09:04:00 +0200

    Implement authentication using next auth credentials - beta

--- auth.config.ts
+++ auth.config.ts
@@ -7,9 +7,9 @@ export const authOptions = {
 	callbacks: {
 		authorized({ auth, request: { nextUrl } }) {
 			const isLoggedIn = !!auth?.user;
-			console.log(isLoggedIn);
 			const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
 			if (isOnDashboard) {
+				if (isLoggedIn) return true;
 				return false;
 			} else if (isLoggedIn) {
 				return Response.redirect(new URL('/dashboard', nextUrl));
